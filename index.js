210a8ad0d6661c3a2e098778805e2797381d33c4
Abhishek Pandey <123861746+abhi759724@users.noreply.github.com>
2023-09-11 05:15:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// create the express instance
+const express = require("express");
+const app = express();
+
+// load the config
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+// middleware the parse the json request body
+app.use(express.json());
+
+// import all the routes
+const todoRoutes = require("./routes/todos");
+
+// mount the api routes
+app.use("/api/v1", todoRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on the PORT: ${PORT}`);
+});
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.get("/", (req, res) => {
+  res.send("<h1>This is Homepage</h1>");
+});
